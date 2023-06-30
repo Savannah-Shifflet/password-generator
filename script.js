3d3c12114e6ea23e04d9e9ad0671bfe30f69bfe1
Savannah Shifflet <savannah.shifflet@gmail.com>
2023-06-30 08:27:25 -0500

    created password length prompt

--- script.js
+++ script.js
@@ -9,38 +9,19 @@ function writePassword() {
 
   passwordText.value = password;
 
-  
-
   password = passwordLength; 
-  
 }
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
 function generatePassword() {
-  // user must enter preferences: 
-    //prompt how many characters
-    // validate it is between 8-128
-    // var for characters length 
+    var passwordLength = window.prompt("How long does your password need to be? (between 8-128 characters)");
+    Number(passwordLength);
+    if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+      window.alert("Please enter a number between 8 and 128");
+    }
 
-    // object for type of character
-    // special, number, upper, lower, use confirm 
-
-  // validation:  
-  // -- if user hit cancel -> alert message
-  // -- if user enters number outside of 8-128, alert that not within 
-  // --if user doesn't select one of the four other criteria, --alert message that at least one character type should be selected 
-
-  // Create arrays for each character type
-
-// take all true arrays and concat into new array
-// choose random chars from arr based on user input
-// var push into new array 
-// var password = array.toString() 
-
-// return password; 
+   
 }
