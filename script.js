26b44f8f05fe53e63fc176d2629edf303c263804
Savannah Shifflet <savannah.shifflet@gmail.com>
2023-07-03 18:36:58 -0500

    added if statements to validate character type choices and add the appropriate choices to the possible characters to use for random selection

--- script.js
+++ script.js
@@ -15,9 +15,10 @@ generateBtn.addEventListener("click", writePassword);
 
 var newCharacter = [];
 var passwordArray = [];
+var randomCharacters = [];
 
 function generatePassword() {
-  
+
   // ask for password length input and make a number type
   var passwordLength = parseInt(window.prompt("How long does your password need to be? (between 8-128 characters)"));
   // validate user entry to ensure it is a number and in the correct range
@@ -32,10 +33,38 @@ function generatePassword() {
   var number = window.confirm("Do you want numbers in your password?");
 
   // identify all possible characters to use in password generation
-  var upperLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var numberList = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var specialCharacters = ["!", "\"", , "\#", "\$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+  var specialCharacters = ["!", "\"", "\#", "\$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 
+  // Based on user's input on character type, create an array with the appropriate character options
+  
+  // validate that one character type was selected
+  if (upper === false && lower === false && special === false && number === false) {
+    window.alert("Please select at least one type of character");
+    generatePassword();
+  } 
+  
+  // add all upperLetters to the full possibility of characters if it was confirmed 
+  if (upper === true) {
+    randomCharacters.push(upperLetters);
+  }
+  
+  // add all lowerLetters to the full possibility of characters if it was confirmed 
+  if (lower === true) {
+    randomCharacters.push(lowerLetters);
+  }
 
+  // add all specialCharacters to the full possibility of characters if it was confirmed 
+  if (special === true) {
+    randomCharacters.push(specialCharacters);
+  }
+  
+  // add all numbers to the full possibility of characters if it was confirmed 
+  if (number === true) {
+    randomCharacters.push(numberList);
+  }
+
+  
 }
